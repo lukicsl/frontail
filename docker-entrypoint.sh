019f908b90e8de875d9a443f75e06c15b48e8759
lukicsl <lukic@gmx.ch>
2019-01-05 17:37:00 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,9 @@ set -euo pipefail
 
 
 if [[ $# -ge 1 ]]; then
+  echo "command parameter supplied"
   exec frontail --disable-usage-stats "$@"
 else
+  echo "Using $LOG_FILES" $LOG_FILES
   exec frontail --disable-usage-stats $LOG_FILES
 fi
