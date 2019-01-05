d0d755a2fb5b24e2e93e5a1e9c7ae43410b8f924
lukicsl <lukic@gmx.ch>
2019-01-05 17:48:54 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [[ $# -ge 1 ]]; then
+  echo "command parameter supplied"
+#  exec frontail --disable-usage-stats "$@"
+else
+  echo "Using $LOG_FILES" $LOG_FILES
+#  exec frontail --disable-usage-stats $LOG_FILES
+fi
